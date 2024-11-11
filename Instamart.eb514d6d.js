0c1c7fc36a6570b1df954d856b1f1c95721d2635
mohid2003 <mohidpathan300@gmail.com>
2024-11-11 17:28:31 +0530

    Updates

---
+++ Instamart.eb514d6d.js
@@ -0,0 +1,2 @@
+var e=globalThis.parcelRequire94c2;(0,e.register)("bdVpV",function(r,t){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>i,set:void 0,enumerable:!0,configurable:!0});var a=e("ayMG0"),i=()=>/*#__PURE__*/(0,a.jsx)("h1",{children:"Instamart"})});
+//# sourceMappingURL=Instamart.eb514d6d.js.map
